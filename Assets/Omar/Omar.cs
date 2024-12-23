1f3089cb8b4568c7ad99e1efa10344be0ec11e61
Amin Abu-Zahra <amin.abuzahra@gmail.com>
2024-12-23 17:25:08 +0200

    Updated

--- Assets/Omar/Omar.cs
+++ Assets/Omar/Omar.cs
@@ -7,12 +7,9 @@ public class Omar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
         Debug.Log("Omar");
         Debug.Log("Subhi");
-=======
-       Debug.Log("Abbas"); 
->>>>>>> 39740854b729d98e991fef07f6035cac56776489
+        Debug.Log("Abbas");
     }
 
     // Update is called once per frame
