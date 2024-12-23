4380b3fd332d781279dd63474e0ab8fab56fb1fc
Amin Abu-Zahra <amin.abuzahra@gmail.com>
2024-12-23 17:24:15 +0200

    first commit

---
+++ Assets/Omar/Omar.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Omar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+<<<<<<< HEAD
+        Debug.Log("Omar");
+        Debug.Log("Subhi");
+=======
+       Debug.Log("Abbas"); 
+>>>>>>> 39740854b729d98e991fef07f6035cac56776489
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
